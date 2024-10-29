6f4fcd7804caef1dd117a4729316fa7b671ebfe8
Luke Horwell <code@horwell.me>
2024-10-29 15:40:46 +0000

    Tidy up JavaScript code
    - Use 'const' variables.
    - Use camel casing.
    - "window.navigator.platform" is deprecated.
    - Attempt detection for all/any via user agent.
    - Remove 'onclick' after clicking distro.

--- assets/polychromatic.js
+++ assets/polychromatic.js
@@ -1,7 +1,4 @@
-var docs_sidebar_shown = false;
-
-
-function _do_smooth_scroll() {
+function scrollTop() {
     window.scroll({
         top: 0,
         left: 0,
@@ -9,153 +6,132 @@ function _do_smooth_scroll() {
     });
 }
 
-const swup = new Swup({
-    animateHistoryBrowsing: true,
-    linkSelector: 'a[href^="/"]:not([data-no-swup]), a[href^="../"]:not([data-no-swup])',
-});
-swup.hooks.on("visit:start", pageExit);
+const swup = new Swup();
 swup.hooks.on("content:replace", pageEnter);
 
-    _do_smooth_scroll();
 function pageEnter() {
+    scrollTop();
 
     // Update navigation 'active' class
-    var nav_class = document.getElementById("current-page-class").value;
-    var nav_active = document.getElementById("nav-" + nav_class);
-    var nav_btns = document.getElementsByClassName("nav-btn");
+    const navClass = document.getElementById("current-page-class").value;
+    const navActive = document.getElementById(`nav-${navClass}`);
 
-    for (b = 0; b < nav_btns.length; b++) {
-        nav_btns[b].classList.remove("active");
-    }
+    document.querySelectorAll(".nav-btn").forEach(function(btn) {
+        btn.classList.remove("active");
+    });
 
-    if (nav_active != null) {
-        nav_active.classList.add("active");
-    }
+    if (navActive)
+        navActive.classList.add("active");
 
     // On mobile, close the navigation menu
     document.getElementById("mobile-menu-toggle").checked = false;
 
-    // Smoothly scroll to the top when clicking internal links
-    var links = document.querySelectorAll("a");
-    for (a = 0; a < links.length; a++) {
-        var link = links[a];
-        var same_domain = link.protocol == window.location.protocol && link.host == window.location.host;
+    // Smoothly scroll to the top for internal links
+    document.querySelectorAll("a").forEach(function(link) {
+        const isSameDomain = link.protocol == window.location.protocol && link.host == window.location.host;
         if (link.smooth == true)
-            continue;
-        if (same_domain != false) {
-            link.addEventListener("click", _do_smooth_scroll);
+            return;
+        if (isSameDomain != false) {
+            link.addEventListener("click", scrollTop);
             link.smooth = true;
         }
-    }
+    });
 
-    // Supported Devices
-    _fetch_device_list("openrazer", "https://openrazer.github.io/api/devices.json", "https://openrazer.github.io/api/latest_version.txt");
+    // Supported Devices page only
+    loadDeviceList("openrazer", "https://openrazer.github.io/api/devices.json", "https://openrazer.github.io/api/latest_version.txt");
 
-    // Downloads --> Distro
-    var bg_accent = document.getElementById("header-accent");
-    var header = document.querySelector(".site-header");
-    if (bg_accent) {
-        header.style.backgroundColor = "rgba(" + bg_accent.value + ", 0.25)";
-    } else {
-        header.style.backgroundColor = null;
-    }
+    // Download subpage only
+    const header = document.querySelector(".site-header");
+    const bgAccent = document.getElementById("header-accent");
+    header.style.backgroundColor = bgAccent ? "rgba(" + bgAccent.value + ", 0.25)" : null;
 
-    // Downloads Index
+    // Downloads page only
     if (window.location.pathname == "/download/")
-        _autodetect_OS();
+        downloadPage();
 }
 
-function _autodetect_OS() {
-    var agent = window.navigator.userAgent;
-    var platform = window.navigator.platform;
+function downloadPage() {
+    const buttons = document.querySelectorAll(".distro");
+    const userAgent = window.navigator.userAgent;
 
-    // Some distros put their name in the user agent (Firefox)
-    if (agent.search("Ubuntu") > 0) {
-        document.querySelector("#ubuntu").classList.add("detected");
-    } else if (agent.search("Fedora") > 0) {
-        document.querySelector("#fedora").classList.add("detected");
-    }
+    // Autodetect OS and highlight the corresponding button
+    buttons.forEach(function(button) {
+        const label = button.querySelector("span").innerText;
+        if (userAgent.indexOf(label) > 0) {
+            button.classList.add("detected");
+        }
+
+        // Keep button highlighted on click
+        button.addEventListener("click", function() {
+            button.classList.add("active");
+        });
+    });
 
     // Show unsupported message for Windows/Mac platforms
-    var possible_win = ["Windows", "Win32", "Win64"];
-    var possible_mac = ["Macintosh", "MacIntel", "MacPPC"];
-    var unsupported_box = document.querySelector("#unsupported-os");
-    var unsupported_img = document.querySelector("#unsupported-os-img");
+    const unsupportedBox = document.querySelector("#unsupported-os");
+    const unsupportedImg = document.querySelector("#unsupported-os-img");
 
-    if (possible_win.indexOf(platform) !== -1) {
-        unsupported_box.style.display = "flex";
-        unsupported_box.hidden = false;
-        unsupported_img.src = "/assets/img/platforms/windows.svg";
+    if (userAgent.indexOf("Windows") > 0) {
+        unsupportedBox.style.display = "flex";
+        unsupportedBox.hidden = false;
+        unsupportedImg.src = "/assets/img/platforms/windows.svg";
     }
 
-    if (possible_mac.indexOf(platform) !== -1) {
-        unsupported_box.style.display = "flex";
-        unsupported_box.hidden = false;
-        unsupported_img.src = "/assets/img/platforms/mac.png";
+    if (userAgent.indexOf("Macintosh") > 0) {
+        unsupportedBox.style.display = "flex";
+        unsupportedBox.hidden = false;
+        unsupportedImg.src = "/assets/img/platforms/mac.png";
     }
 }
 
-function _fetch_device_list(backend_id, devices_url, version_url) {
-    if (! document.getElementById(backend_id + "-device-table"))
+function loadDeviceList(backend_id, devices_url, version_url) {
+    const table = document.getElementById(`${backend_id}-device-table`);
+    const info = document.getElementById(`${backend_id}-info`);
+
+    if (!table)
         return;
 
-    var xhttp = new XMLHttpRequest();
-    var xhttp2 = new XMLHttpRequest();
+    const requestDevices = new XMLHttpRequest();
+    const requestVersion = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() {
+    requestDevices.onreadystatechange = function() {
         if (this.readyState != 4)
             return;
 
-        if (this.status != 200 && this.status != 304)
-            return _failed_device_list("openrazer");
+        if (this.status != 200 && this.status != 304) {
+            info.innerText = "Unable to retrieve data. Please visit OpenRazer's website for the list of supported devices.";
+            return;
+        }
 
         if (this.readyState == 4 && this.status == 200) {
-            var devices = JSON.parse(xhttp.responseText);
-            return _process_device_list("openrazer", devices);
+            const devices = JSON.parse(requestDevices.responseText);
+
+            devices.forEach(function(device) {
+                const formfactor = device.type.charAt(0).toUpperCase() + device.type.slice(1);
+                const row = table.insertRow(-1);
+
+                const name = row.insertCell(0);
+                name.innerHTML = `<img src="/assets/img/formfactor/${device.type}.svg" title="${formfactor}"/> ${device.name}`;
+
+                const vidpid = row.insertCell(1);
+                vidpid.innerHTML = device.vid + ":" + device.pid;
+                if (device.alias_ids && device.alias_ids.length > 0)
+                    vidpid.innerHTML += `, ${device.alias_ids.join(", ")}`;
+            });
         }
     };
 
-    xhttp2.onreadystatechange = function() {
+    requestVersion.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200 || this.status == 304) {
-            var info = document.getElementById(backend_id + "-info");
-            info.innerText = `Listing devices for OpenRazer ${xhttp2.responseText}`;
+            info.innerText = `Listing devices for OpenRazer ${requestVersion.responseText}`;
         }
     };
 
-    xhttp.open("GET", devices_url);
-    xhttp.send();
-    xhttp2.open("GET", version_url);
-    xhttp2.send();
+    requestDevices.open("GET", devices_url);
+    requestDevices.send();
+    requestVersion.open("GET", version_url);
+    requestVersion.send();
 }
 
-function _failed_device_list(backend_id) {
-    var info = document.getElementById(backend_id + "-info");
-    info.innerText = "Unable to retrieve data. Please visit OpenRazer's website for the list of supported devices.";
-}
-
-function _process_device_list(backend_id, devices) {
-    var table = document.getElementById(backend_id + "-device-table");
-    for (d = 0; d < devices.length; d++) {
-        var device = devices[d];
-        var formfactor = device.type.charAt(0).toUpperCase() + device.type.slice(1);
-
-        var row = table.insertRow(-1);
-        var name = row.insertCell(0);
-        var vidpid = row.insertCell(1);
-        name.innerHTML = `<img src="/assets/img/formfactor/${device.type}.svg" title="${formfactor}"/> ${device.name}`;
-        vidpid.innerHTML = device.vid + ":" + device.pid;
-        if (device.alias_ids && device.alias_ids.length > 0)
-            vidpid.innerHTML += `, ${device.alias_ids.join(", ")}`;
-    }
-}
-
-function pageExit() {
-
-}
-
-function download_selected(element) {
-    element.classList.add("active");
-}
-
-// Initial page load
+// Initial site load
 pageEnter();
